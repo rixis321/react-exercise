644c674e2010449b5c1d47d7cd5f09b4f4764733
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-09-22 04:27:45 +0200

    carousel class done, need to do event listener

--- src/Details.js
+++ src/Details.js
@@ -1,21 +1,39 @@
 import { useParams } from "react-router-dom";
 import { Component } from "react";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor(props) {
+  /*  constructor(props) {
     super(props);
 
     this.state = { loading: true };
+  }*/
+
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+
+    /*this.setState({ loading: false });*/
+    /*this.setState(json.pets[0]);*/
   }
+
   render() {
     if (this.state.loading) {
       return <h2> loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
@@ -29,4 +47,8 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+export default WrappedDetails;
